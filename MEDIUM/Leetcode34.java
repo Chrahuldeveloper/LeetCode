f9c451366fb92214667a8aabd72b15787bfbda12
Rahul <119114775+Chrahuldeveloper@users.noreply.github.com>
2025-10-15 23:42:58 +0530

    Add searchRange method to find target range
    Implement searchRange method to find target indices.

--- MEDIUM/Leetcode34.java
+++ MEDIUM/Leetcode34.java
@@ -1,1 +1,24 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
 
+        int[] res = new int[2];
+
+        res[0] = -1;
+        res[1] = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+         if(nums[i] == target){
+            res[1] = i;
+         }
+        }
+
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if(nums[j] == target){
+                res[0] = j;
+            }
+        }
+
+        return res;
+
+    }
+}
